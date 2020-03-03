3957df67bfa4dcbd67df4eb87df2079d56157b4c
brandonstiles1 <brandonstiles@gmail.com>
2020-03-02 23:44:36 -0500

    Commit 5; everything reduced & working

--- packages/landing-gatsby/src/containers/AppClassic/FeatureSlider/featureSlider.style.js
+++ packages/landing-gatsby/src/containers/AppClassic/FeatureSlider/featureSlider.style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Mockup from 'common/src/assets/image/app/iphone-mockup.png';
+import Mockup from 'common/src/assets/image/appClassic/iphone-mockup.png';
 
 const FeatureSliderWrapper = styled.div`
   position: relative;
